44f11a185a04ce4dc33b971ec77a1acbdf2d4479
davesiwachok <dave@handymanchicago.net>
2015-10-25 10:33:56 -0500

    Create screenshot.py

---
+++ screenshot.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+
+driver = webdriver.Firefox()
+driver.get('http://www.github.com/')
+driver.save_screenshot('github.png')
+driver.quit()
+#Requires selenium module
+#Opens Browser
+#Goes to website
+#Saves screenshot as .png file in same directory as this file
+#Closes browser
