30fdd082cef54569cc0742e467d88fe79cb77106
davesiwachok <dave@handymanchicago.net>
2015-10-25 11:02:17 -0500

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -4,7 +4,7 @@ driver = webdriver.Firefox()
 driver.get('http://www.github.com/')
 driver.save_screenshot('github.png')
 driver.quit()
-#Requires selenium module
+#Requires selenium 
 #Opens Browser
 #Goes to website
 #Saves screenshot as .png file in same directory as this file
